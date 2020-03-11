658ce89c447ef0d1a4cc2755ba5b67ec12435735
Mevin G Monson <mevingmonson@gmail.com>
2020-03-11 14:58:05 +0530

    time component

--- github-file-list/src/components/FileListItem.js
+++ github-file-list/src/components/FileListItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import FileIcon from './FileIcon'
 import CommitMessage from './CommitMessage'
+import Time from './Time'
 
 
 const FileListItem = ({ file }) => (
@@ -9,6 +10,9 @@ const FileListItem = ({ file }) => (
         <FileIcon file={file} />
         <td className="file-name">{file.name}</td>
         <CommitMessage commit={file.latestCommit} />
+        <td className="age">
+            <Time time={file.updated_at} />
+        </td>
     </tr>
 );
 
