502600dc7a8a9bbccdba4259178b1dc1ce58a73a
Mevin G Monson <mevingmonson@gmail.com>
2020-03-22 22:37:42 +0530

    react guide

---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components'
+// import Radium from 'radium'
+
+// import './Person.css';
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+
+    @media (min-width: 500px) {
+        color:blue;
+        width:450px;
+    }
+`;
+
+const Person = (props) => {
+
+    // const style = {
+    //     '@media (min-width:500px)': {
+    //         width: '450px'
+    //     }
+    // };
+
+    return (
+        // <div className="Person" style={style}>
+        <StyledDiv>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </StyledDiv>
+        //  </div >
+    )
+};
+
+// export default Radium(Person);
+export default Person;
